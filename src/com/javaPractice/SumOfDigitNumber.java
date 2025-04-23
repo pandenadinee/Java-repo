14a5041d4e8a892a112d81c17f38c2da2cfca71c
Apple <apple@Apples-Laptop.local>
2025-04-23 12:27:17 +0530

    adding modified file

--- src/com/javaPractice/SumOfDigitNumber.java
+++ src/com/javaPractice/SumOfDigitNumber.java
@@ -2,7 +2,7 @@ package com.javaPractice;
 
 public class SumOfDigitNumber {
     public static void main(String[] args) {
-        int num = 2345;
+        int num = 12345;
         int sum = 0;
         while (num>0){
             int rem = num % 10;
--- src/com/program/RightRotateArray.java
+++ src/com/program/RightRotateArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class RightRotateArray {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1,2,3,4,5,6,7,8};
         int n = arr.length;
         int k = 2;
         int count = k % n;
